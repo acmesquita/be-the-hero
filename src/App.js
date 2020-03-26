e2a0de402dbb8cfba967f67e1512127d06235c96
Catharina Mesquita <catha.ana.1994@gmail.com>
2020-03-26 19:57:33 -0300

    Adicionando rotas no projeto

--- src/App.js
+++ src/App.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 import './global.css';
 
-import Logon from './pages/Logon';
+import Routes from './router'
 
 export default function App() {
   return (
-    <Logon />
+    <Routes />
   );
 }
