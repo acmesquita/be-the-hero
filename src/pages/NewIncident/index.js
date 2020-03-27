04030efebd735ba8aca39639ed1d96fe7e429efd
Catharina Mesquita <catha.ana.1994@gmail.com>
2020-03-27 19:37:36 -0300

    Criando funcao de cadastrar um novo incidente

--- src/pages/NewIncident/index.js
+++ src/pages/NewIncident/index.js
@@ -1,11 +1,45 @@
 import React from 'react';
 import { FiArrowLeft } from 'react-icons/fi';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 
 import './styles.css';
 import Logo from '../../assets/logo.svg';
+import { useState } from 'react';
+
+import api from '../../services/api'
 
 export default function NewIncident() {
+
+  const [title, setTitle] = useState('')
+  const [description, setDescription] = useState('')
+  const [value, setValue] = useState('')
+
+  const ongId = localStorage.getItem('ong_id')
+
+  const history = useHistory()
+
+  async function handleNewIncident(e) {
+    e.preventDefault();
+
+    const data = {
+      title,
+      description,
+      value
+    }
+    try {
+      await api.post('/incidents', data, {
+        headers: {
+          Authorization: ongId,
+        }
+      })
+
+      history.push('/profile')
+    } catch (error) {
+      alert('Deu erro ao cadastrar um novo incidente.')
+    }
+
+  }
+
   return (
     <div className="new-incident-container">
       <div className="content">
@@ -19,10 +53,22 @@ export default function NewIncident() {
             Voltar para home
           </Link>
         </section>
-        <form>
-          <input placeholder="Titulo do caso" />
-          <textarea placeholder="Descrição" />
-          <input placeholder="Valor em reais" />
+        <form onSubmit={handleNewIncident}>
+          <input
+            placeholder="Titulo do caso"
+            value={title}
+            onChange={e => { setTitle(e.target.value) }}
+          />
+          <textarea
+            placeholder="Descrição"
+            value={description}
+            onChange={e => setDescription(e.target.value)}
+          />
+          <input
+            placeholder="Valor em reais"
+            value={value}
+            onChange={e => { setValue(e.target.value) }}
+          />
           <div className="button-group">
             <Link className="button-default" to="/profile">Cancelar</Link>
             <button className="button" type="submit">Cadastrar</button>
